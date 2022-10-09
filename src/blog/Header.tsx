85c06b57711127c1076fb509019ca3bca123b4a7
codewithsakshi <sakshimittal3005@gmail.com>
2022-10-09 11:16:24 +0530

    first commit

---
+++ src/blog/Header.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Signup from "../component/Signup"
+import { Sign } from 'crypto';
+import SignUp from '../component/Signup';
+
+interface HeaderProps {
+  sections: ReadonlyArray<{
+    title: string;
+    url: string;
+  }>;
+  title: string;
+}
+
+export default function Header(props: HeaderProps) {
+  const { sections, title } = props;
+  const [isModalOpen, setIsModalOpen] = React.useState(false)
+
+  const handleOpen = () => {
+     setIsModalOpen(true)
+  }
+
+  const handleClose = () => {
+    setIsModalOpen(false)
+ }
+
+  return (
+    <React.Fragment>
+      <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Button size="small">Subscribe</Button>
+        <Typography
+          component="h2"
+          variant="h5"
+          color="inherit"
+          align="center"
+          noWrap
+          sx={{ flex: 1 }}
+        >
+          {title}
+        </Typography>
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+        <Button variant="outlined" size="small" onClick={handleOpen}>
+          Sign up
+        </Button>
+         {isModalOpen && <SignUp open={handleOpen} handleClose={handleClose}></SignUp>}
+      </Toolbar>
+      <Toolbar
+        component="nav"
+        variant="dense"
+        sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
+      >
+        {sections.map((section) => (
+          <Link
+            color="inherit"
+            noWrap
+            key={section.title}
+            variant="body2"
+            href={section.url}
+            sx={{ p: 1, flexShrink: 0 }}
+          >
+            {section.title}
+          </Link>
+        ))}
+      </Toolbar>
+    </React.Fragment>
+  );
+}
